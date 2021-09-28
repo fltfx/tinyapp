0f5cb1ef10cf20b1faf3d6f8b39adeb73b9c4681
C W <claudiaywong@msn.com>
2021-09-28 01:21:57 +0000

    added functionality to receive form in put in urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -4,11 +4,19 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+//middleware
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let randomStr = Math.random().toString(36).slice(7);
+  return randomStr;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,9 +35,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  //const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
